50b04cc3ab1c3e69fd9d4aefe5378d04575952f7
BetoRobert <wyjawy@hotmail.com>
2024-06-19 04:16:31 -0500

    Agregamos la autenticacion

--- app/page.tsx
+++ app/page.tsx
@@ -5,23 +5,24 @@ import { generateClient } from "aws-amplify/data";
 import type { Schema } from "@/amplify/data/resource";
 import "./../app/app.css";
 import { Amplify } from "aws-amplify";
-import outputs from "@/amplify_outputs.json";
+import outputs from "@/amplify_outputs.json";   //se encarga de leerlo
 import "@aws-amplify/ui-react/styles.css";
+import {Authenticator} from '@aws-amplify/ui-react'  //Esto lo importamos para utilizar la authentication
 
-Amplify.configure(outputs);
-
-const client = generateClient<Schema>();
+Amplify.configure(outputs); // la cadena de coneccion - the blockchain jjj
+ 
+const client = generateClient<Schema>(); //la coneccion a base de datos
 
 export default function App() {
   const [todos, setTodos] = useState<Array<Schema["Todo"]["type"]>>([]);
-
+  //y toda la informacion viene desde models
   function listTodos() {
-    client.models.Todo.observeQuery().subscribe({
+    client.models.Todo.observeQuery().subscribe({   //esto actualiza la informacion cada vez que cambia el backend
       next: (data) => setTodos([...data.items]),
     });
   }
 
-  useEffect(() => {
+  useEffect(() => { //inicialmente el carga las listas de tareas
     listTodos();
   }, []);
 
@@ -31,15 +32,25 @@ export default function App() {
     });
   }
 
+  function deleteTodo(id: string) {
+    client.models.Todo.delete({id})  //client es la base de datos 
+  
+  }
+ //Este componente viene desde  @aws-amplify/ui-react  --> <Authenticator>  
   return (
-    <main>
+    <Authenticator>  
+    {({signOut, user})=>(
+      <main>
       <h1>My todos</h1>
       <button onClick={createTodo}>+ new</button>
       <ul>
         {todos.map((todo) => (
-          <li key={todo.id}>{todo.content}</li>
+          <li key={todo.id}
+              onClick={() => deleteTodo(todo.id)}
+          >{todo.content}</li>
         ))}
       </ul>
+      <button onClick={signOut}>Cerrar Sesion</button>
       <div>
         🥳 App successfully hosted. Try creating a new todo.
         <br />
@@ -48,5 +59,7 @@ export default function App() {
         </a>
       </div>
     </main>
+    )}
+    </Authenticator>
   );
 }
